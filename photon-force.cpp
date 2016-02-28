c23597e78019aaa398889d866db999959381abd7
Peter Horrocks <p.horrocks@marathon-targets.com>
2016-02-28 20:48:12 +1100

    Comment change

--- photon-force.cpp
+++ photon-force.cpp
@@ -23,12 +23,12 @@ PRODUCT_VERSION(2);
 // easily. This table shows the assignments, however, and hopefully they stay
 // relatively in-sync with reality.
 //
-// Function         Requires  Pin     TIM DMA1
-// ------------------------------------------
-//                            D0/PB7
-// IR send          timer/DMA D1/PB6  4.1 0.2
-// Trigger          interrupt D2/PB5
-// NeoPixel circle  timer/DMA D3/PB4  3.1 4.5
+// Function         Requires  Pin      TIM  DMA1
+// --------------------------------------------
+//                            D0/PB7  (4.2)
+// IR send          timer/DMA D1/PB6   4.1  0.2
+// Trigger          interrupt D2/PB5  (3.2)
+// NeoPixel circle  timer/DMA D3/PB4   3.1  4.5
 // IR recv (enable) -         D4/PB3
 // IR recv (clk)    -         D5/PA15
 // Haptic           -         D6/PA14
@@ -38,10 +38,10 @@ PRODUCT_VERSION(2);
 // IR recv (input1) 5V        A1/PC3
 // IR recv (input2) 5V        A2/PC2
 // Power (enable)   -         A3/PA5
-// IR recv (input3) 5V        A4/PA6
-// IR recv (input4) 5V        A5/PA7
-// Audio            DAC/DMA   DAC/PA4 7   5.7
-//                            WKP/PA0
+// IR recv (input3) 5V        A4/PA6  (3.1)
+// IR recv (input4) 5V        A5/PA7  (3.2)
+// Audio            DAC/DMA   DAC/PA4  7    5.7
+//                            WKP/PA0 (5.1)
 
 // Note: interrupt not available on D0
 
